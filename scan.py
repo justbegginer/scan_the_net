ca18b8fb733bc5e5c773895b6dd646b26c06b959
justbegginer <blackcloudsinthesky@protonmail.com>
2019-11-21 01:43:19 +0300

    get response and parse it

--- scan.py
+++ scan.py
@@ -16,12 +16,23 @@ def scan(ip):
     #print(arp_request.summary()) #print Arp request
     #print(arp_request.show())#show args
     broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff") #make broadcast req
-    #print(broadcast.summary())
-    #print(broadcast.show())
-    broadcast_req = broadcast/arp_request# / means concatenation
+    broadcast_request = broadcast/arp_request# / means concatenation
     #scapy.ls(scapy.Ether) #information about current class
+    answered = scapy.srp(broadcast_request ,timeout = 1)[0] # the second element of array is a unanswered list
+    #print(answered.summary())
+    return parse_resp(answered)
 
-scan("192.168.0.1")
-#scan("192.168.0.1/24")# /24 -> that part doesn't work
+def parse_resp(response):
+    book = {}
+    for i in response:
+        ip = i[1].psrc#parse ip
+        mac_address = i[1].hwsrc# parse mac
+        # how i parse? i is an element of response , it contains sent information and get information
+        book[ip] = mac_address
+    return book
+
+#scan("192.168.0.1")
+print(scan("192.168.0.1/24"))# /24 -> that part doesn't work correct like ARP who has ?? says ??
+
 #for i in range(1 , 256):
 #    scan("192.168.0."+str(i))
